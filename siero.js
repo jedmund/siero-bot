d111462c994a21604d67c48f6ed83aac1230b71a
Justin Edmund <justin@jedmund.com>
2019-01-28 15:24:52 -0500

    Cleaned up and added comments

--- siero.js
+++ siero.js
@@ -1,11 +1,14 @@
+// Set up environment variables
+require('dotenv').config()
+
+// Set up Discord
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+// Let us know when Siero has connected to a server
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
 })
 
-bot_secret_token = ""
-
 // Log in to Discord with the secret token
-client.login(bot_secret_token)
+client.login(process.env.DISCORD_SECRET)
