12fdba16eb57cc9d4dbf89a946b1cedb906dff82
Justin Edmund <justin@jedmund.com>
2019-01-28 16:07:18 -0500

    Added ability to respond to messages

--- siero.js
+++ siero.js
@@ -10,5 +10,38 @@ client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
 })
 
+// Respond to messages
+client.on('message', (receivedMessage) => {
+    // Siero shouldn't respond to her own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+
+    if (receivedMessage.content.startsWith("!")) {
+        processCommand(receivedMessage)
+    }
+})
+
+function processCommand(receivedMessage) {
+    // Remove the leading exclamation mark
+    let fullCommand = receivedMessage.content.substr(1)
+
+    // Split the message up into pieces for each space
+    let splitCommand = fullCommand.split(" ")
+
+    // Get the first word directly after the exclamation mark
+    let primaryCommand = splitCommand[0]
+
+    // Separate all other arguments or parameters
+    let arguments = splitCommand.slice(1)
+
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments)
+
+    if (primaryCommand == "spark") {
+        sparkCommand(arguments, receivedMessage)
+    }
+}
+
 // Log in to Discord with the secret token
 client.login(process.env.DISCORD_SECRET)
