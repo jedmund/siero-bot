097cc4d55da568ef63f626c9810568c88cb6006e
Justin Edmund <justin@jedmund.com>
2019-02-05 21:45:09 -0500

    Streamline startup db code

--- connect.js
+++ connect.js
@@ -2,30 +2,33 @@ const sqlite3 = require('sqlite3').verbose();
  
 // open database from file
 let db = new sqlite3.Database('./db/siero.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the Knickknack Shack.');
+    if (err) {
+        console.error(err.message);
+    }
+
+    console.log('Connected to the Knickknack Shack.');
 });
 
-let sql = 
+let createTableSql = 
 `CREATE TABLE IF NOT EXISTS sparks (
     user_id TEXT PRIMARY KEY,
-    crystals INTEGER,
-    tickets INTEGER,
-    ten_tickets INTEGER,
+    crystals INTEGER DEFAULT 0,
+    tickets INTEGER DEFAULT 0,
+    ten_tickets INTEGER DEFAULT 0,
     target TEXT,
     last_updated TEXT
 )`
- 
-db.serialize(() => {
-    db.run(sql)
-})
 
-// close the database connection
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+db.run(createTableSql, (err) => {
+    if (err) {
+        console.log(err)
+    }
+
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+
+        console.log('Close the database connection.');
+    })
+})
