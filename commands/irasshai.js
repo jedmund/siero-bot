57e5ed3d1c3b8ca823dbf77b45f17b29d3c994f1
Justin Edmund <justin@jedmund.com>
2019-02-10 05:29:34 -0500

    Added premium irasshai

---
+++ commands/irasshai.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord-akairo')
+const { RichEmbed } = require('discord.js')
+
+class IrasshaiCommand extends Command {
+    constructor() {
+        super('irasshai', {
+            aliases: ['irasshai', 'hello']
+        })
+    }
+
+    exec(message, args) {
+        var embed = new RichEmbed()
+        embed.setColor(0xcb4b16)
+        embed.setTitle("いらっしゃい！")
+        embed.setDescription("旅支度はシャロちゃんにおまかせ〜\nhttp://game-a5.granbluefantasy.jp/assets_en/sound/voice/3050002000_v_206.mp3")
+        embed.setImage("https://i.imgur.com/ZTT4uCO.jpg")
+
+        message.channel.send(embed)
+    }
+}
+
+module.exports = IrasshaiCommand
