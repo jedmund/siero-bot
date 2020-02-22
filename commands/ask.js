3f7eae4b1333e54e0a85f97ab30a4f2a8083715f
Justin Edmund <justin@jedmund.com>
2020-02-22 15:42:03 -0800

    Added an ask command

---
+++ commands/ask.js
@@ -0,0 +1,31 @@
+const { Command } = require('discord-akairo')
+
+const Answers = [
+    "Yes",
+    "Possibly",
+    "What do you think?",
+    "I don't think so",
+    "Hmm... maybe!",
+    "Fufufu~",
+    "Definitely not",
+    "Absolutely",
+    "Not a chance",
+    "No way",
+    "Are you sure that's the right question?"
+]
+
+class AskCommand extends Command {
+    constructor() {
+        super('ask', {
+            aliases: ['ask'],
+            trigger: ['<(@539533389187776523)>']
+        })
+    }
+
+    exec(message) {
+        console.log(message)
+        message.reply(Answers[Math.floor(Math.random() * Answers.length)])
+    }
+}
+
+module.exports = AskCommand
