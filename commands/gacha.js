47cf46c4f913673e64a95057addc9fd8a7f8bc29
Justin Edmund <justin@jedmund.com>
2020-03-01 19:02:14 -0800

    Cleaned up the gacha command

--- commands/gacha.js
+++ commands/gacha.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg')
 const { Command } = require('discord-akairo')
+const { Gacha } = require('../services/gacha.js')
 const { RichEmbed } = require('discord.js')
 
 const client = getClient()
@@ -30,17 +31,15 @@ class GachaCommand extends Command {
     }
 
     exec(message, args) {
-        this.storeBanner(args)
-
         switch(args.operation) {
             case "yolo":
                 this.yolo(message, args)
                 break
             case "ten":
-                this.ten_pull(message)
+                this.ten_pull(message, args)
                 break
             case "spark":
-                this.spark(message)
+                this.spark(message, args)
                 break
             case "help":
                 this.help(message)
@@ -52,94 +51,27 @@ class GachaCommand extends Command {
 
     // Command methods
     yolo(message, args) {
-        var rarity = this.determineRarity(false)
+        let gacha = new Gacha(args.gala, args.season)
+        let item = gacha.singleRoll()
+        let response = this.responseString(item)
 
-        let sql = this.sqlString(1)
-        client.query(sql, [rarity.int], (err, res) => {
-            if (err) {
-                console.log(err.message)
-            }
-
-            let response = this.responseString(res.rows[0])
-            message.reply(response)
-        })
+        message.reply(response)
     }
 
-    ten_pull(message) {
-        // Determine which pull will guarantee an SR or above
-        let guaranteedRateUpPull = this.randomIntFromInterval(1, 10)
-
-        // Roll the gacha to determine rarity
-        var roll = this.tenPartRoll()
+    ten_pull(message, args) {
+        let gacha = new Gacha(args.gala, args.season)
+        let items = gacha.tenPartRoll()
+        var response = `You got these 10 things!\`\`\`html\n${this.multilineResponseString(items.items)}\n\`\`\``
         
-        var sql = this.multiSqlString(roll)
-        client.query(sql, (err, res) => {
-            if (err) {
-                console.log(err.message)
-            }
-
-            var string = `You got these 10 things! \`\`\`html\n${this.multilineResponseString(res.rows)}\n\`\`\``
-
-            message.reply(string)
-        })
+        message.reply(response)
     }
 
-    spark(message) {
-        // Roll the gacha to determine rarity
-        let rollsInSpark = 30
-        var rolls = this.tenPartRoll(rollsInSpark)
-        
-        var sql = this.multiSqlString(rolls, true)
-        client.query(sql, (err, res) => {
-            if (err) {
-                console.log(err.message)
-           }
+    spark(message, args) {
+        let gacha = new Gacha(args.gala, args.season)
+        let items = gacha.spark()
 
-            var embed = new RichEmbed()
-            embed.setColor(0xb58900)
-            
-            var result = ""
-            for (var i in res.rows) {
-                result += this.responseString(res.rows[i], true)
-            }
-            
-            let rate = Math.floor((rolls.SSR / 300) * 100)
-            
-            embed.setDescription("```html\n" + result + "\n```")
-            embed.addField("Summary", `\`\`\`${this.summaryString(res.rows, rolls)}\`\`\``)
-            embed.setFooter(`Your SSR rate is ${rate}%`)
-            
-            result += `\nYour SSR rate is ${rate}%!`
-
-            message.channel.send(embed)
-        })
-    }
-
-    tenPartRoll(times = 1) {
-        // Create an object to store counts
-        var count = { 
-            R: 0, 
-            SR: 0, 
-            SSR: 0 
-        }
-
-        for (var i = 0; i < times; i++) {
-            // Determine which pull will guarantee an SR or above
-            let guaranteedRateUpPull = this.randomIntFromInterval(1, 10)
-
-            // Determine how many items of each rarity to retrieve
-            for (var j = 0; j < 10; j++) {
-                if (i == guaranteedRateUpPull) {
-                    var rarity = this.determineRarity(true)
-                } else {
-                    var rarity = this.determineRarity(false)
-                }
-
-                count[rarity.string] += 1
-            }
-        }
-
-        return count
+        let embed = this.buildEmbed(items)
+        message.channel.send(embed)
     }
 
     help(message) {
@@ -165,92 +97,13 @@ The <season> you choose adds seasonal SSRs to the pool\`\`\``)
         message.channel.send(embed)
     }
 
-    // Gacha methods
-    currentRates() {
-        var rates = {}
-        if (["flash", "ff", "legend", "lf"].includes(this.gala)) {
-            rates = {
-                "R":   0.76,
-                "SR":  0.15,
-                "SSR": 0.06
-            }
-        } else {
-            rates = {
-                "R":   0.82,
-                "SR":  0.15,
-                "SSR": 0.03
-            }
-        }
-    
-        return rates
-    }
-
-    determineRarity(isRateUp = false) {
-        let rates = this.currentRates()
-        var rNum = Math.random()
-
-        var rarity = {
-            integer: 0,
-            string: ""
-        }
-
-        if (rNum < rates.SSR) {
-            rarity.int = 3
-            rarity.string = "SSR"
-        } else if (rNum < rates.SR) {
-            rarity.int = 2
-            rarity.string = "SR"
-        } else {
-            if (isRateUp) {
-                rarity.int = 2
-                rarity.string = "SR"
-            } else {
-                rarity.int = 1
-                rarity.string = "R"
-            }
-        }
-
-        return rarity
-    }
-
+    // Filter methods
     filterSSRWeapons(el) {
         return el.rarity == 3 && el.item_type == 0
     }
     
     filterSSRSummons(el) {
         return el.rarity == 3 && el.item_type == 1
-    }
-
-    limitBanner() {
-        var additionalSql = "AND (premium = 1"
-    
-        // Test the gala
-        if (["flash", "ff"].includes(this.gala)) {
-            additionalSql += " OR flash = 1"
-        } 
-        
-        else if (["legend", "lf"].includes(this.gala)) {
-            additionalSql += " OR legend = 1"
-        }
-    
-        // Test the season
-        if (this.season == "valentine") {
-            additionalSql += " OR valentine = 1"
-        }
-    
-        else if (this.season == "summer") {
-            additionalSql += " OR summer = 1"
-        }
-    
-        else if (this.season == "halloween") {
-            additionalSql += " OR halloween = 1"
-        }
-    
-        else if (this.season == "holiday") {
-            additionalSql += " OR holiday = 1"
-        }
-    
-        return additionalSql + ")"
     }
     
     sortCharacterWeapons(results) {
@@ -295,29 +148,7 @@ The <season> you choose adds seasonal SSRs to the pool\`\`\``)
         return characterWeapons
     }
 
-    // String methods
-    sqlString(times) {
-        let constraints = this.limitBanner()
-        return `SELECT * FROM gacha WHERE rarity = $1 ${constraints} ORDER BY RANDOM() LIMIT ${times};`
-    }
-    
-    multiSqlString(counts, isSpark = false) {
-        var sql = ""
-        var constraints = this.limitBanner()
-
-        let ssrSql = `(SELECT * FROM gacha WHERE rarity = 3 ${constraints} ORDER BY RANDOM() LIMIT ${counts.SSR})`
-    
-        if (!isSpark) {
-            let rSql = `(SELECT * FROM gacha WHERE rarity = 1 ${constraints} ORDER BY RANDOM() LIMIT ${counts.R})`
-            let srSql = `(SELECT * FROM gacha WHERE rarity = 2 ${constraints} ORDER BY RANDOM() LIMIT ${counts.SR})`
-            sql = [rSql, srSql, ssrSql].join(" UNION ALL ")
-        } else {
-            sql = ssrSql
-        }
-    
-        return sql
-    }
-    
+    // String methods    
     responseString(result, combined = false) {
         var response = ""
     
@@ -362,6 +193,25 @@ The <season> you choose adds seasonal SSRs to the pool\`\`\``)
     
         return string
     }
+
+    buildEmbed(results) {
+        console.log(results)
+        var embed = new RichEmbed()
+        embed.setColor(0xb58900)
+
+        var response = ""
+        for (var i in results.items) {
+            response += this.responseString(results.items[i], true)
+        }
+            
+        let rate = Math.floor((results.count.SSR / 300) * 100)
+            
+        embed.setDescription("```html\n" + response + "\n```")
+        embed.addField("Summary", `\`\`\`${this.summaryString(results.items, results.count)}\`\`\``)
+        embed.setFooter(`Your SSR rate is ${rate}%`)
+
+        return embed
+    }
     
     summaryString(results, count) {
         let ssrWeapons = results.filter(this.filterSSRWeapons)
@@ -376,10 +226,6 @@ The <season> you choose adds seasonal SSRs to the pool\`\`\``)
     }
 
     // Helper methods
-    randomIntFromInterval(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-    
     // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
     shuffle(array) {
         var currentIndex = array.length, temporaryValue, randomIndex
@@ -398,11 +244,6 @@ The <season> you choose adds seasonal SSRs to the pool\`\`\``)
         }
       
         return array
-    }
-
-    storeBanner(args) {
-        this.gala = args.gala
-        this.season = args.season
     }
 }
 
