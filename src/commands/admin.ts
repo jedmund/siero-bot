ba3e72c8a9a470c8bcff0cc2d1261c48d818325a
Justin Edmund <justin@jedmund.com>
2020-05-24 01:44:58 -0700

    Added an admin command for me :)

---
+++ src/commands/admin.ts
@@ -0,0 +1,22 @@
+import { Message } from "discord.js"
+
+const { Command } = require('discord-akairo')
+
+const pluralize = require('pluralize')
+
+class AdminCommand extends Command {
+    constructor() {
+        super('admin', {
+            aliases: ['admin']
+        })
+    }
+
+    exec(message: Message) {
+        if (message.author.id === '139468879008235520') {
+            const stats = `Currently running for ${this.client.users.cache.size} ${pluralize('user', this.client.users.cache.size)} in ${this.client.guilds.cache.size} ${pluralize('server', this.client.guilds.cache.size)}.`
+            message.channel.send(stats)
+        }
+    }
+}
+
+module.exports = AdminCommand
