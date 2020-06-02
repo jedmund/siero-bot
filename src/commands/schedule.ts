80778c2801400b9051e9d4de6e0f37bf99171378
Justin Edmund <justin@jedmund.com>
2020-06-02 03:59:12 -0700

    Cleaned up $schedule
    There's still some things that are erroring in Typescript

--- src/commands/schedule.ts
+++ src/commands/schedule.ts
@@ -1,24 +1,55 @@
 
-import { Message, MessageEmbed } from 'discord.js'
+import { Message } from 'discord.js'
+import { MessageEmbed } from 'discord.js'
 import { Command } from 'discord-akairo'
 import { promises as fs } from 'fs'
 
-// import common from '../helpers/common.js'
+import common from '../helpers/common.js'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
+
 dayjs.extend(relativeTime)
 dayjs.extend(localizedFormat)
 
 const path = require('path')
 
+interface Event {
+    readonly [index: string]: string | LocalizedString | null
+    name: LocalizedString
+    type: string
+    starts: string
+    ends: string
+    advantage: string | null
+}
 
+interface Month {
+    month: string
+    period: string
+    events: {
+        en: string[]
+        jp: string[]
+    }
+}
+
+interface Schedule {
+    events: Event[]
+    scheduled: Month[]
+}
+
+interface LocalizedString {
+    en: string
+    jp: string
+}
 
 class ScheduleCommand extends Command {
-    schedule: { [key: string]: [ { [key: string]: string }] } = {}
     message: Message
+    schedule: Schedule = {
+        events: [],
+        scheduled: []
+    }
 
-    constructor() {
+    public constructor() {
         super('schedule', {
             aliases: ['schedule', 'sc'],
             args: [
@@ -32,8 +63,9 @@ class ScheduleCommand extends Command {
     }
 
     public async exec(message: Message, args: { operation: string }) {
-        this.message = message
         console.log(`(${dayjs().format('YYYY-MM-DD HH:mm:ss')}) [${message.author.id}] ${message.content}`)
+
+        common.storeMessage(this, message)
 
         await this.load()
             .then(() => {
@@ -46,12 +78,19 @@ class ScheduleCommand extends Command {
             case 'show':
                 this.show()
                 break
+
             case 'next':
                 this.next()
                 break
+
             case 'current':
                 this.current()
                 break
+
+            case 'now':
+                this.current()
+                break
+
             default:
                 break
         }
@@ -59,59 +98,82 @@ class ScheduleCommand extends Command {
 
     // Command methods
     private async show() {
-        let embed = this.renderSchedule()
+        const embed: MessageEmbed = this.renderSchedule()
         this.message.channel.send(embed)
     }
 
-    private next() {
-        const embed = this.renderEvent(this.schedule.events[1], false)
+    private next(): void {
+        const index: number = this.extractCurrentIndex() + 1
+        const event: Event = this.schedule.events[index]
+        const embed: MessageEmbed = this.renderEvent(event, false)
+
         this.message.channel.send(embed)
     }
 
-    private current() {
-        const embed = this.renderEvent(this.schedule.events[0])
+    private current(): void {
+        const index: number = this.extractCurrentIndex()
+        const event: Event = this.schedule.events[index]
+        const embed: MessageEmbed = this.renderEvent(event)
+
         this.message.channel.send(embed)
     }
 
     // File methods
-    private async load() {
-        const schedule = path.join(__dirname, '..', '..', '..', 'src', 'resources', 'schedule.json')
-        const data = await fs.readFile(schedule, 'utf8')
+    private async load(): Promise<void> {
+        const schedule: string = path.join(__dirname, '..', '..', '..', 'src', 'resources', 'schedule.json')
+        const data: string = await fs.readFile(schedule, 'utf8')
 
         this.schedule = JSON.parse(data)
     }
 
     // Render methods
-    private renderSchedule() {
-        let embed = new MessageEmbed({
+    private renderSchedule(): MessageEmbed {
+        const events: Event[] = this.schedule.events
+        
+        let embed: MessageEmbed = new MessageEmbed({
             title: 'Schedule'
         })
+
+        for (let i in events) {
+            let event: Event = events[i]
+            
+            const isCurrentEvent: boolean = (dayjs(event.starts).isBefore(dayjs()) && dayjs(event.ends).isAfter(dayjs()))
+            const startsString: string = `Starts ${dayjs().to(event.starts)}\n${dayjs(event.starts).format('LLLL')} JST`
+            const endsString: string = `Ends ${dayjs().to(event.ends)}\n${dayjs(event.ends).format('LLLL')} JST`
+            const dateString: string = (isCurrentEvent) ? endsString : startsString
+
+            let duration: string = `\`\`\`html${dateString}\`\`\``
+            embed.addField(event.name.en, duration)
+        }
 
         return embed
     }
 
-    private renderEvent(event: { [key: string]: string }, currentEvent: boolean = true) {
+    private renderEvent(event: Event, currentEvent: boolean = true): MessageEmbed {
         let embed = new MessageEmbed({
             color: 0xdc322f
         })
 
         const keys = Object.keys(event)
         for (var i = 0; i < keys.length; i++) {
-            const key = keys[i]
-            const readableKey = this.capitalize(keys[i].replace('-', ' '))
+            const key: string = keys[i]
+            const readableKey: string = this.capitalize(keys[i].replace('-', ' '))
 
             if (currentEvent && key !== 'starts') {
-                
+                // do nothing
             }
 
             if (!currentEvent && key === 'starts') {
-                const formattedTo = dayjs().to(event[key])
-                const formattedTime = dayjs(event[key]).format('LLLL')
+                const formattedTo: string = dayjs().to(event[key])
+                const formattedTime: string = dayjs(event[key]).format('LLLL')
+
                 embed.addField(`${readableKey} ${formattedTo}`, `${formattedTime} JST`)
             }
 
             if (key === 'banner') {
-                embed.setImage(event[key])
+                if (typeof event[key] === "string") {
+                    embed.setImage(event[key] as string)
+                }
             }
 
             if (key === 'name') {
@@ -123,26 +185,50 @@ class ScheduleCommand extends Command {
             }
             
             if (currentEvent && key === 'ends') {
-                const formattedTo = dayjs().to(event[key])
-                const formattedTime = dayjs(event[key]).format('LLLL')
+                const formattedTo: string = dayjs().to(event[key])
+                const formattedTime: string = dayjs(event[key]).format('LLLL')
+
                 embed.addField(`${readableKey} ${formattedTo}`, `${formattedTime} JST`)
             }
 
             if (!currentEvent && key === 'ends') {
-                const formattedTime = dayjs(event[key]).format('LLLL')
+                const formattedTime: string = dayjs(event[key]).format('LLLL')
                 embed.addField(readableKey, `${formattedTime} JST`)
             }
             
-            if (key === 'advantage') {
-                embed.addField(readableKey, this.capitalize(event[key]))
+            if (key === 'advantage' && event[key]) {
+                const advantage: string = event[key]!
+                embed.addField(readableKey, this.capitalize(advantage))
             }
         }
 
         return embed
     }
 
-    private capitalize(string: string) {
+    // Helper methods
+    private capitalize(string: string): string {
         return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    private extractCurrentIndex(): number {
+        let found: boolean = false
+        let n: number = 0
+
+        while (!found) {
+            const event: Event = this.schedule.events[n]
+
+            const startsBeforeNow: boolean = dayjs(event.starts).isBefore(dayjs())
+            const endsAfterNow: boolean = dayjs(event.ends).isAfter(dayjs())
+
+            if (startsBeforeNow && endsAfterNow) {
+                found = true
+                return n
+            }
+
+            n++
+        }
+
+        return n
     }
 }
 
