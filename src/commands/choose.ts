55e899cbceefe179fc85a55ca618b47f23467b32
Justin Edmund <justin@jedmund.com>
2020-06-02 05:38:01 -0700

    Added a quick choose command

---
+++ src/commands/choose.ts
@@ -0,0 +1,38 @@
+import { Message } from 'discord.js'
+import { Command } from 'discord-akairo'
+
+class ChooseCommand extends Command {
+    constructor() {
+        super('choose', {
+            aliases: ['choose', 'pick', 'ch'],
+            args: [
+                {
+                    id: 'options',
+                    type: 'string'
+                }
+            ]
+        })
+    }
+
+    exec(message: Message) {
+        console.log(`[${message.author.id}] ${message.content}`)
+
+        let options: string[] = this.parseRequest(message.content)
+        const reply = options[Math.floor(Math.random() * options.length)]
+
+        message.reply(`Hmm... I choose ${reply}!`)
+    }
+
+    private parseRequest(request: string): string[] {
+        const splitRequest = request.split(' ').splice(1).join('').split(',')
+
+        let options: string[] = []
+        for (let i in splitRequest) {
+            options.push(splitRequest[i].trim())
+        }
+
+        return options
+    }
+}
+
+module.exports = ChooseCommand
