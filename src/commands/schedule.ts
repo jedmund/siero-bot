70101a6d18b182cc9239e6d478651af8ab3bf0e5
Justin Edmund <justin@jedmund.com>
2020-06-05 01:19:06 -0700

    Schedule should only show events that haven't ended

--- src/commands/schedule.ts
+++ src/commands/schedule.ts
@@ -176,14 +176,16 @@ class ScheduleCommand extends Command {
 
         for (let i in events) {
             let event: Event = events[i]
-            
-            const isCurrentEvent: boolean = (dayjs(event.starts).isBefore(dayjs()) && dayjs(event.ends).isAfter(dayjs()))
-            const startsString: string = `<Starts in ${dayjs().to(event.starts, true)}>\n${dayjs(event.starts).format('LLLL')} JST`
-            const endsString: string = `<Ends in ${dayjs().to(event.ends, true)}>\n${dayjs(event.ends).format('LLLL')} JST`
-            const dateString: string = (isCurrentEvent) ? endsString : startsString
 
-            let duration: string = `\`\`\`html\n${dateString}\n\`\`\``
-            embed.addField(event.name.en, duration)
+            if (dayjs(event.ends).isAfter(dayjs())) {
+                const isCurrentEvent: boolean = (dayjs(event.starts).isBefore(dayjs()) && dayjs(event.ends).isAfter(dayjs()))
+                const startsString: string = `<Starts in ${dayjs().to(event.starts, true)}>\n${dayjs(event.starts).format('LLLL')} JST`
+                const endsString: string = `<Ends in ${dayjs().to(event.ends, true)}>\n${dayjs(event.ends).format('LLLL')} JST`
+                const dateString: string = (isCurrentEvent) ? endsString : startsString
+
+                let duration: string = `\`\`\`html\n${dateString}\n\`\`\``
+                embed.addField(event.name.en, duration)
+            }
         }
 
         return embed
