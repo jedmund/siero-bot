6c45e03e2694038889a9331d85d74d8f46ee6729
Justin Edmund <justin@jedmund.com>
2020-06-11 11:32:50 -0700

    Added extra fields back to $help

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,7 +6,7 @@ const dayjs = require('dayjs')
 class HelpCommand extends Command {
     constructor() {
         super('help', {
-            aliases: ['help', 'h'],
+            aliases: ['help'],
             args: []
         })
     }
@@ -20,16 +20,24 @@ class HelpCommand extends Command {
             color: 0xdc322f
         })
 
+        const discord = 'Come help with development or get support in Siero\'s Discord:\nhttps://discord.gg/37u2uz7'
+
         if (message.channel.type !== 'dm') {
             await common.fetchPrefix(message.guild.id)
                 .then((prefix) => {
                     const commands = this.helpContent(prefix)
                     embed.addField('Commands', commands)
+                    embed.addField('Even more help', `You can get help on any of the above commands by typing \`help\` after the command, like so: 
+\`\`\`${prefix}gacha help\`\`\``)
+                    embed.addField('Support Discord', discord)
                     message.channel.send(embed)
                 })
         } else {
             const commands = this.helpContent()
             embed.addField('Commands', commands)
+            embed.addField('Even more help', `You can get help on any of the above commands by typing \`help\` after the command, like so: 
+\`\`\`$gacha help\`\`\``)
+            embed.addField('Support Discord', discord)
             message.channel.send(embed)
         }
     }
