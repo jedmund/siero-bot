9f6ec5747e1aa54ee138c6a2730c27731d3735d7
Justin Edmund <justin@jedmund.com>
2020-06-14 12:55:05 -0700

    XML parser for abilities

--- src/classes/character.ts
+++ src/classes/character.ts
@@ -1,6 +1,10 @@
 import { Ability, AbilityNumber, AbilityDescription, Duration, SupportAbility, Ougi, Stat } from '../services/constants'
 import { Element, Gender, Race, Rarity, Specialty, UnitType } from '../services/constants'
 
+const xml2js = require('xml2js')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+
 const util = require('util')
 
 interface BilingualObject {
@@ -15,6 +19,7 @@ interface CharacterStat {
     ougiCount: number
 }
 
+type AnyResult = { [key: string]: any }
 type StringResult = { [key: string]: String }
 
 export class Character {
@@ -59,8 +64,30 @@ export class Character {
     abilities: Ability[] = []
     supportAbilities: SupportAbility[] = []
 
-    public static parse(parts: any): Character {
-        let char: Character = new Character()
+    xml: string
+    dom = new JSDOM('')
+
+    public constructor(xml: string) {
+        this.xml = xml
+    }
+
+    public async extract(xml: string): Promise<AnyResult> {
+        const parser = new xml2js.Parser({
+            explicitArray: false,
+            normalize: true
+        })
+
+        return await parser.parseStringPromise(xml)
+            .then((result: any) => {
+                return result.root.template[1].part
+            })
+            .catch((error: Error) => {
+                console.log(error)
+            })
+    }
+
+    public async parse() {
+        let parts = await this.extract(this.xml)
 
         for (let i in parts) {
             const part = parts[i]
@@ -69,80 +96,81 @@ export class Character {
 
             switch(name) {
                 case 'id':
-                    char.id = value
+                    this.id = value
                     break
                 case 'name':
-                    char.name.en = value
+                    this.name.en = value
                     break
                 case 'jpname':
-                    char.name.jp = value
+                    this.name.jp = value
                     break
                 case 'title': 
-                    char.title.en = value
+                    this.title.en = value
                     break
                 case 'jptitle': 
-                    char.title.jp = value
+                    this.title.jp = value
                     break
                 case 'va':
-                    char.seiyuu.en = value
+                    this.seiyuu.en = value
                     break
                 case 'jpva': 
-                    char.seiyuu.jp = value
+                    this.seiyuu.jp = value
                     break
                 case 'release_date':
-                    char.releaseDate = value
+                    this.releaseDate = value
                     break
                 case 'rarity':
-                    char.rarity = this.mapRarity(value)
+                    this.rarity = this.mapRarity(value)
                     break
                 case 'element':
-                    char.element = this.mapElement(value)
+                    this.element = this.mapElement(value)
                     break
                 case 'race':
-                    char.race = this.mapRace(value)
+                    this.race = this.mapRace(value)
                     break
                 case 'weapon':
-                    char.specialty = this.mapSpecialty(value)
+                    this.specialty = this.mapSpecialty(value)
                     break
                 case 'type':
-                    char.type = this.mapUnitType(value)
+                    this.type = this.mapUnitType(value)
                     break
                 case 'min_atk':
-                    char.stats.atk.min = parseInt(value)
+                    this.stats.atk.min = parseInt(value)
                     break
                 case 'max_atk':
-                    char.stats.atk.max = parseInt(value)
+                    this.stats.atk.max = parseInt(value)
                     break
                 case 'flb_atk':
-                    char.stats.atk.flb = parseInt(value)
+                    this.stats.atk.flb = parseInt(value)
                     break
                 case 'min_hp':
-                    char.stats.hp.min = parseInt(value)
+                    this.stats.hp.min = parseInt(value)
                     break
                 case 'max_hp':
-                    char.stats.hp.max = parseInt(value)
+                    this.stats.hp.max = parseInt(value)
                     break
                 case 'flb_hp':
-                    char.stats.hp.flb = parseInt(value)
+                    this.stats.hp.flb = parseInt(value)
                     break
                 case 'abilitycount':
-                    char.stats.abilityCount = parseInt(value)
+                    this.stats.abilityCount = parseInt(value)
                     break
                 case 'ougi_count':
-                    char.stats.ougiCount = parseInt(value)
+                    this.stats.ougiCount = parseInt(value)
                     break
                 default:
                     break
             }
         }
 
-        char.ougis = this.parseOugis(char.stats.ougiCount, parts)
-        char.abilities = this.parseAbilities(char.stats.abilityCount, parts)
-
-        return char
+        // this.ougis = this.parseOugis(this.stats.ougiCount, parts)
+        this.abilities = this.parseAbilities(this.stats.abilityCount, parts)
     }
 
-    private static parseAbilities(count: number, parts: any): Ability[] {
+    private parseAbilities(count: number, parts: any): Ability[] {
+        // Parse out ability descriptions
+        const abilityDescriptions = this.extractAbilitiesFromXml()
+
         let abilities: Ability[] = []
 
         for (let i = 0; i < count; i++) {
@@ -151,20 +179,18 @@ export class Character {
             let nameKey = `a${index}_name`
             let cooldownKey = `a${index}_cd`
             let durationKey = `a${index}_dur`
-            let descriptionKey = `a${index}_effdesc`
 
-            const keys = [nameKey, cooldownKey, durationKey, descriptionKey]
-            abilities.push(this.parseAbility(keys, parts))
+            const keys = [nameKey, cooldownKey, durationKey]
+            abilities.push(this.parseAbility(keys, parts, abilityDescriptions[i]))
         }
 
         return abilities
     }
 
-    private static parseAbility(keys: string[], parts: any): Ability {
+    private parseAbility(keys: string[], parts: any, descriptions: AbilityDescription[]): Ability {
         const found = parts.filter((part: any) => keys.includes(part.name))
         
         let name: string = ''
-        let descriptions: AbilityDescription[] = []
 
         let cooldown: AbilityNumber = {
             initial: 0,
@@ -214,26 +240,6 @@ export class Character {
                         }
                     }
                 }
-            } else if (item.name.includes('effdesc')) {
-                // console.log(item.value.template.part)
-
-                const numDescriptions = parseInt(item.value.template.part[0].value) + 1
-
-                for (let i = 0; i < numDescriptions; i++) {
-                    let levelKey = ''
-                    let descriptionKey = ''
-
-                    if (i == 0) {
-                        levelKey = 'level'
-                        descriptionKey = 'des'
-                    } else {
-                        levelKey = `level${i}`
-                        descriptionKey = `des${i}`
-                    }
-
-                    const keys = [levelKey, descriptionKey]
-                    descriptions.push(this.parseDescription(keys, item.value.template.part))
-                }
             }
         }
 
@@ -245,37 +251,76 @@ export class Character {
         }
     }
 
-
-    private static parseDescription(keys: string[], parts: any): AbilityDescription {
-        const found = parts.filter((part: any) => keys.includes(part.name))
+    public extractAbilitiesFromXml(): AbilityDescription[][] {
+        const inputXml = `<?xml version="1.0" encoding="UTF-8"?>${this.xml}`
+        const domParser: DOMParser = new this.dom.window.DOMParser()
+        const parsed: XMLDocument = domParser.parseFromString(inputXml, 'application/xml')
         
-        let level: number = 0
-        let description: string = ''
+        const rootNode = parsed.getElementsByTagName('root')[0]
+        const targetNode: Node = rootNode.childNodes[1]
 
-        for (let i in found) {
-            const item = found[i]
+        return this.extractAbilities(targetNode)
+    }
 
-            if (item.name.includes('level')) {
-                level = parseInt(item.value)
-            } else if (item.name.includes('des')) {
-                // console.log(util.inspect(item.value, false, null, true /* enable colors */))
-                if (item.value._) {
-                    // TODO: When an ability or ougi has linked statuses, 
-                    // we are going to have to rebuild them.
-                    description = this.br2nl(item.value._)
+    public extractAbilities(target: Node): AbilityDescription[][] {
+        const xpath1: string = '//template/part/name[contains(text(), "effdesc")]'
+        const descriptionParentNode = this.dom.window.document.evaluate(xpath1, target, null, 7, null)
+
+        let ads: AbilityDescription[][] = []
+
+        for (let i = 0; i < descriptionParentNode.snapshotLength; i++) {
+            let node = descriptionParentNode.snapshotItem(i)
+
+            if (node && node.parentNode) {
+                // make abilitydescription
+                let texts: string[] = this.extractXMLData('des', node.parentNode) as string[]
+                let levels: number[] = this.extractXMLData('level', node.parentNode) as number[]
+
+                ads.push(this.mergeAbilityDescriptionsWithUpgradeLevels(texts, levels))
+            }
+        }
+
+        return ads
+    }
+    
+    private mergeAbilityDescriptionsWithUpgradeLevels(descriptions: string[], levels: number[]): AbilityDescription[] {
+        let abilityDescriptions: AbilityDescription[] = []
+        for (let i = 0; i < descriptions.length; i++) {
+            const level = (i > 0 && levels[i - 1]) ? levels[i - 1] : 0
+
+            abilityDescriptions.push({
+                text: descriptions[i],
+                level: level
+            })
+        }
+
+        return abilityDescriptions
+    }
+
+    public extractXMLData(key: string, target: Node,): Array<string | number> {
+        const xpath: string = `.//part/name[contains(text(), "${key}")]`
+        const targetNode = this.dom.window.document.evaluate(xpath, target, null, 7, null)
+            
+        let values = []
+        for (let i = 0; i < targetNode.snapshotLength; i++) {
+            let node = targetNode.snapshotItem(i)
+            if (node && node.parentNode && node.parentNode.querySelector('value')) {
+                let childList = node.parentNode.querySelector('value')?.childNodes
+                let valueArray = [''].slice.call(childList)
+                let fragment = valueArray.reduce(this.reduceNode, '')
+
+                if (parseInt(fragment)) {
+                    values.push(parseInt(fragment))
                 } else {
-                    description = 'Effect enhanced.'
+                    values.push(this.br2nl(fragment))
                 }
             }
         }
 
-        return {
-            text: description,
-            level: level
-        }
+        return values
     }
 
-    private static parseOugis(count: number, parts: any): Ougi[] {
+    private parseOugis(count: number, parts: any): Ougi[] {
         let ougis: Ougi[] = []
 
         for (let i = 0; i < count; i++) {
@@ -302,7 +347,7 @@ export class Character {
         return ougis
     }
 
-    private static parseOugi(keys: string[], parts: any): Ougi {
+    private parseOugi(keys: string[], parts: any): Ougi {
         const found = parts.filter((part: any) => keys.includes(part.name))
         
         let name: string = ''
@@ -330,7 +375,8 @@ export class Character {
         }
     }
 
-    private static mapElement(value: string) {
+    // Mapping methods
+    private mapElement(value: string) {
         const element = value.toLowerCase()
 
         switch(element) {
@@ -351,7 +397,7 @@ export class Character {
         }
     }
 
-    private static mapRace(value: string) {
+    private mapRace(value: string) {
         const race = value.toLowerCase()
 
         switch(race) {
@@ -370,7 +416,7 @@ export class Character {
         }
     }
 
-    private static mapRarity(rarity: string) {
+    private mapRarity(rarity: string) {
         switch(rarity) {
             case 'SSR':
                 return 3
@@ -383,7 +429,7 @@ export class Character {
         }
     }
 
-    private static mapSpecialty(value: string) {
+    private mapSpecialty(value: string) {
         const values = value.toLowerCase().split(',')
 
         let specialties: Specialty[] = []
@@ -430,7 +476,7 @@ export class Character {
         return specialties
     }
 
-    private static mapUnitType(value: string) {
+    private mapUnitType(value: string) {
         const unitType = value.toLowerCase()
 
         switch(unitType) {
@@ -449,7 +495,29 @@ export class Character {
         }
     }
 
-    private static br2nl(str: string): string {
+    // Helper methods
+    private br2nl(str: string): string {
         return str.replace(/<br\s*\/?>/mg,"\n");
+    }
+
+    public clean(): any {
+        const { dom, xml, ...char } = this
+        return char
+    }
+
+    private reduceNode(previousValue: string, currentValue: any): string {
+        const node: Node = currentValue as Node
+        const accumulatedValue: string = previousValue
+        let string = ''
+
+        if (node.nodeName !== 'ext') {
+            if (node.nodeType === 1 && node.childNodes[1].textContent) {
+                string = node.childNodes[1].textContent
+            } else if (node.nodeType === 3 && node.textContent) {
+                string = node.textContent
+            }
+        }
+
+        return accumulatedValue + string
     }
 }
