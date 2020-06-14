4478e9452adc54ae73355955c59e1d1e35df3d43
Justin Edmund <justin@jedmund.com>
2020-06-14 12:54:52 -0700

    Clean up $wiki

--- src/services/wiki.ts
+++ src/services/wiki.ts
@@ -1,7 +1,6 @@
 import { Character } from '../classes/character'
 
 const MWBot = require('mwbot')
-const xml2js = require('xml2js')
 
 const util = require('util')
 
@@ -28,65 +27,15 @@ export class Wiki {
             prop: 'parsetree'
         }
 
-        const parser = new xml2js.Parser({
-            explicitArray: false,
-            normalize: true
-        })
-
         this.bot.request(options)
             .then((response: AnyResult) => {
                 return response.parse.parsetree['*']
             })
             .then((xml: string) => {
-                console.log(xml)
-                return parser.parseStringPromise(xml)
-            })
-            .then((result: AnyResult) => {
-                const parts: any = result.root.template[1].part
-                const character: Character = Character.parse(parts)
-                // console.log(util.inspect(character, false, null, true /* enable colors */))
+                const character: Character = new Character(xml)
+                character.parse().then(() => {
+                    console.log(util.inspect(character.clean(), false, null, true))
+                })
             })
     }
-
-    // private emptyCharacter() {
-    //     return {
-    //         id: '',
-    //         name: {
-    //             en: '',
-    //             jp: ''
-    //         },
-    //         title: {
-    //             en: '',
-    //             jp: ''
-    //         },
-    //         seiyuu: {
-    //             en: '',
-    //             jp: ''
-    //         },
-    //         gender: Gender.female,
-    //         releaseDate: '',
-    //         element: Element.null,
-    //         rarity: Rarity.R,
-    //         type: UnitType.attack,
-    //         race: Race.other,
-    //         specialty: [],
-    //         stats: {
-    //             atk: {
-    //                 min: 0,
-    //                 max: 0,
-    //                 flb: 0
-    //             },
-    //             hp: {
-    //                 min: 0,
-    //                 max: 0,
-    //                 flb: 0
-    //             },
-    //             abilityCount: 0,
-    //             ougiCount: 0
-    //         },
-    //         ougis: [],
-    //         abilities: [],
-    //         supportAbilities: []
-    //     }
-    // }
 }
