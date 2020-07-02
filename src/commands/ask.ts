bb44c8d47b32a1f791f6e150a7112cb57ce4a5cf
Justin Edmund <justin@jedmund.com>
2020-07-02 02:47:00 -0700

    Migrate and update ask.ts
    $ask will now hash the question with the user's ID. This means that she can give the same answer to identical questions when asked by the same user.

--- src/commands/ask.ts
+++ src/commands/ask.ts
@@ -1,34 +1,41 @@
-import { Message } from "discord.js"
+import { Message } from 'discord.js'
+import { SieroCommand } from '../helpers/SieroCommand'
 
-const { Command } = require('discord-akairo')
-
-const Answers = [
-    "Yes",
-    "Possibly",
-    "What do you think?",
-    "I don't think so",
-    "Hmm... maybe!",
-    "Fufufu~",
-    "Hehehe!",
-    "Definitely not",
-    "Absolutely",
-    "Not a chance",
-    "No way",
-    "Are you sure that's the right question?"
+const Answers: string[] = [
+    'Yes',
+    'Possibly',
+    'What do you think?',
+    'I don\'t think so',
+    'Hmm... maybe!',
+    'Fufufu~',
+    'Hehehe!',
+    'Definitely not',
+    'Absolutely',
+    'Not a chance',
+    'No way',
+    'Are you sure that\'s the right question?'
 ]
 
-class AskCommand extends Command {
-    constructor() {
+class AskCommand extends SieroCommand {
+    public constructor() {
         super('ask', {
             aliases: ['ask']
         })
     }
 
-    exec(message: Message) {
-        console.log(`[${message.author.id}] ${message.content}`)
-        
-        const reply = Answers[Math.floor(Math.random() * Answers.length)]
-        message.reply(reply)
+    public exec(message: Message) {
+        this.log(message)
+
+        const hash = this.hash(message.author.id + message.content, Answers.length)
+        message.reply(Answers[hash])
+    }
+
+    private hash(string: string, size: number): number {
+        let hash = 0
+        for (let x = 0; x < string.length; x++) {
+            hash += string.charCodeAt(x)
+        }
+        return (hash % size)
     }
 }
 
