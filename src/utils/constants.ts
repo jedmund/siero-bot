8b13ee4110a851614c4648b14dae0c8329557218
Justin Edmund <justin@jedmund.com>
2023-05-01 09:41:00 -0700

    Add basic types and constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const SSR_RATE = 0.03
+export const ROLLS_IN_SPARK = 300
+export const ROLLS_IN_TENPART = 10
---
+++ src/utils/types.ts
@@ -0,0 +1,5 @@
+import DrawableItem from "../interfaces/DrawableItem"
+
+export type CategoryMap = { [key: string]: Category }
+export type ItemMap = { [key: number]: DrawableItem[] }
+export type RateMap = { [key: string]: number }
