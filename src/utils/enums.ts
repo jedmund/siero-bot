8bedfce26fc74db007503db124426e1d479ec848
Justin Edmund <justin@jedmund.com>
2023-05-01 09:40:36 -0700

    Add enums
    These are enums that we use in commands and services

---
+++ src/utils/enums.ts
@@ -0,0 +1,41 @@
+export enum DrawableItemType {
+  WEAPON = 0,
+  SUMMON = 1,
+}
+
+export enum GachaBucket {
+  WEAPON = 1,
+  SUMMON = 2,
+  LIMITED = 3,
+  RATEUP = 4,
+}
+
+export enum Promotion {
+  PREMIUM = "premium",
+  CLASSIC = "classic",
+  LEGEND = "legend",
+  FLASH = "flash",
+}
+
+export enum Rarity {
+  R = 1,
+  SR = 2,
+  SSR = 3,
+}
+
+export enum Element {
+  NULL = 0,
+  WIND = 1,
+  FIRE = 2,
+  WATER = 3,
+  EARTH = 4,
+  DARK = 5,
+  LIGHT = 6,
+}
+
+export enum Season {
+  VALENTINES = "valentines",
+  SUMMER = "summer",
+  HALLOWEEN = "halloween",
+  HOLIDAY = "holiday",
+}
