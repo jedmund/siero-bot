3644110922839a8aa8a96b62c680298f36646583
Justin Edmund <justin@jedmund.com>
2023-05-03 06:35:08 -0700

    Update types for RateMaps

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,4 +2,14 @@ import DrawableItem from "../interfaces/DrawableItem"
 
 export type CategoryMap = { [key: string]: Category }
 export type ItemMap = { [key: number]: DrawableItem[] }
-export type RateMap = { [key: string]: number }
+export type RateMap = {
+  identifier: string
+  rate: number
+}[]
+export type ItemRateMap = {
+  item: DrawableItem
+  rate: number
+}[]
+export type RarityRateMap = {
+  [key: string]: number
+}
