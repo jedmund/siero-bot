44875e42dbcdb0896bc126db99efc83cef7a1f31
Justin Edmund <justin@jedmund.com>
2025-03-28 16:31:41 -0400

    Updated existing interfaces

--- src/interfaces/DrawableItem.d.ts
+++ src/interfaces/DrawableItem.d.ts
@@ -1,18 +1,17 @@
 import { DrawableItemType, Rarity } from "../utils/enums"
 
 export default interface DrawableItem {
-  [index: string]: any | string | number | boolean | null
   id: string
   item_id: string
   granblue_id: string
   type: DrawableItemType
   name: {
-    [index: string]: string
     en: string
     jp: string
   }
   recruits?: Character
   rarity: Rarity
+  element: Element
   promotions: {
     premium: boolean
     classic: boolean
--- src/interfaces/RawResult.d.ts
+++ src/interfaces/RawResult.d.ts
@@ -1,24 +1,29 @@
 interface RawResult {
   id: string | null
-  character_id: string | null
-  character_name_en: string | null
-  character_name_jp: string | null
-  character_granblue_id: string | null
-  character_rarity: number | null
-  character_element: number | null
+  drawable_type: string | null
+  drawable_id?: string | null
+
   weapon_id: string | null
   weapon_name_en: string | null
   weapon_name_jp: string | null
   weapon_granblue_id: string | null
   weapon_rarity: number | null
   weapon_element: number | null
+
+  character_id: string | null
+  character_name_en: string | null
+  character_name_jp: string | null
+  character_granblue_id: string | null
+  character_rarity?: number | null
+  character_element?: number | null
+
   summon_id: string | null
   summon_name_en: string | null
   summon_name_jp: string | null
   summon_granblue_id: string | null
   summon_rarity: number | null
   summon_element: number | null
-  drawable_type: string | null
+
   premium: boolean | null
   classic: boolean | null
   flash: boolean | null
@@ -27,4 +32,6 @@ interface RawResult {
   summer: boolean | null
   halloween: boolean | null
   holiday: boolean | null
+
+  rate?: number | null
 }
