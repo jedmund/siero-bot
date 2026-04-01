da89371c34a146b09d1c6d4c0ec698ab8eed20d2
Justin Edmund <justin@jedmund.com>
2026-04-01 19:59:37 -0400

    switch to ESM with NodeNext module resolution

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
+import "dotenv/config"
 import { LogLevel, SapphireClient } from "@sapphire/framework"
 import { GatewayIntentBits } from "discord.js"
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config()
-}
 
 const sapphire = new SapphireClient({
   intents: [
